efa42b0915cd9654a33641bbe2fc8a698e549e3e
andrewarthur16 <72278858+andrewarthur16@users.noreply.github.com>
2024-05-23 21:02:44 -0300

    Add database connection

---
+++ database.php
@@ -0,0 +1,13 @@
+
+<?php
+
+$conexao = mysqli_connect("127.0.0.1:3306", "root", "", "formulario_cad");
+
+// if (mysqli_query($conexao, "CREATE DATABASE IF NOT EXISTS formulario_cad")) {
+//     echo 'Banco de dados criado';
+//     }
+//     else {
+//     echo 'Erro criando o banco de dados';
+//     }
+?>
+
